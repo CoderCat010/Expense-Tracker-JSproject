ae5541ae1e80396a92b230c5ff0a83a22e282f58
Ummay Nusra <nusraumme@gmail.com>
2026-06-10 20:39:45 +0600

    added all id

---
+++ script.js
@@ -0,0 +1,16 @@
+// storing each one id 
+const balance_El = document.getElementById('balance');
+
+const income_El = document.getElementById('income-amount');
+
+const expenses_El = document.getElementById('expenses-amount');
+
+const transaction_list = document.getElementById('transaction-list');
+
+const transaction_Form = document.getElementById('transaction-form');
+
+const description_InputBox = document.getElementById('description-inputBox');
+
+const amount_InputBox = document.getElementById('amount-inputBox');
+
+// add event listenter on transaction form
