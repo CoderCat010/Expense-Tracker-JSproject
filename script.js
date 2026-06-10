cf6dc753a10ad00c44f4a1eee6d2248d8d896ae8
Ummay Nusra <nusraumme@gmail.com>
2026-06-10 20:51:42 +0600

    got values from descriction & amount input box

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 // storing each one id 
 const balance_El = document.getElementById('balance');
-
 const income_El = document.getElementById('income-amount');
-
 const expenses_El = document.getElementById('expenses-amount');
-
 const transaction_list = document.getElementById('transaction-list');
-
 const transaction_Form = document.getElementById('transaction-form');
-
 const description_InputBox = document.getElementById('description-inputBox');
-
 const amount_InputBox = document.getElementById('amount-inputBox');
 
 // add event listenter on transaction form
@@ -18,4 +12,9 @@ transaction_Form.addEventListener('submit', addTransaction);
 
 function addTransaction(event){
     event.preventDefault();
+
+    // get values from description & amount input boxes 
+    const description = description_InputBox.value.trim();
+    const amount = parseFloat(amount_InputBox.value);
+    
 }
